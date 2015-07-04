d3796001a4aba079c944d74ae8242d0e870bc30e
lushl9301 <lushl9301@gmail.com>
2015-07-04 15:36:05 +0800

    format objdir

--- core/kernel/objdir.c
+++ core/kernel/objdir.c
@@ -17,17 +17,18 @@
 #include <stdc.h>
 #include <processor.h>
 
-const struct object *objectTab[OBJ_NO_CLASSES]={[0 ... OBJ_NO_CLASSES-1] = 0};
+const struct object *objectTab[OBJ_NO_CLASSES] = {[0... OBJ_NO_CLASSES - 1] = 0};
 
-void SetupObjDir(void) {
-    extern xm_s32_t (*objectSetupTab[])(void);
-    xm_u32_t e;
-    for (e=0; objectSetupTab[e]; e++) {
-	if (objectSetupTab[e])
-	    if ((objectSetupTab[e]())<0) {
-                cpuCtxt_t ctxt;
-                GetCpuCtxt(&ctxt);
-		SystemPanic(&ctxt, "[ObjDir] Error setting up object at 0x%x\n", objectSetupTab);
-            }
-    }
+void SetupObjDir(void)
+{
+	extern xm_s32_t (*objectSetupTab[])(void);
+	xm_u32_t e;
+	for (e = 0; objectSetupTab[e]; e++) {
+		if (objectSetupTab[e])
+			if ((objectSetupTab[e]()) < 0) {
+				cpuCtxt_t ctxt;
+				GetCpuCtxt(&ctxt);
+				SystemPanic(&ctxt, "[ObjDir] Error setting up object at 0x%x\n", objectSetupTab);
+			}
+	}
 }
